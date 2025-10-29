062303d0e2ebef02793e28788af93b2eb8879db0
Elijah Chamberlain <elicvideogamer@gmail.com>
2025-10-29 13:40:20 -0700

    Update attendanceApp.java

--- attendanceApp.java
+++ attendanceApp.java
@@ -1,27 +1,49 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class attendanceApp {
-      public void main(){
+      public static void main(String[] args){
 
-          Dog dogs[] = {new Dog("Benny", 3), new Dog("Floof", 1)}
-          Cat cats[] = {new Cat("Seth", false), new Cat("Garfield", true)}
+            Dog dogs[] = {new Dog("Benny", 3), new Dog("Floof", 1)}
+            Cat cats[] = {new Cat("Seth", false), new Cat("Garfield", true)}
 
-          Animal dogsAndCats[] = new dogsAndCats[dogs.length() + cats.length()];
+            Animal dogsAndCats[] = new dogsAndCats[dogs.length() + cats.length()];
 
-          int i = 0;
-          int j = 0;
-          for( i < dogsAndCats.length()){
-              if( i < dogs.lenght()){
-                  dogsAndCats[i] = dogs[i];
-                  i++;
+            int i = 0;
+            int j = 0;
+            for( i < dogsAndCats.length()){
+                  if( i < dogs.lenght()){
+                        dogsAndCats[i] = dogs[i];
+                        i++;
               }
-              else{
+            else{
                 dogsAndCats[i] = dogs[j];
                 i++;
                 j++;
               }
           }
 
-          System.out.println(dogs[]);
-          System.out.println(cats[]);
-          System.out.println(dogsAndCats[]);
+            System.out.println(dogs[]);
+            System.out.println(cats[]);
+            System.out.println(dogsAndCats[]);
+
+            List<Dog> dogList = new ArrayList<>();
+
+            dogList.add(new Dog("Benny", 3))
+            dogList.add(new Dog("Floof", 1))
+
+            List<cat> catList = new ArrayList<>();
+
+            catList.add(new Cat("Seth", false))
+            catList.add(new Cat("Garfield", true)))
+
+            List<Animal> dogsAndCatsList = new ArrayList<>();
+
+            dogsAndCats.add(dogList);
+            dogsAndCats.add(catList);
+
+            System.out.println(dogsList);
+            System.out.println(catList);
+            System.out.println(dogsAndCatsList);
       }
 }
